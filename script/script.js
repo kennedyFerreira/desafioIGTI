446d78a3e2974b37279cdcc6d4ff22503174cdba
kennedyFerreira <kennedsantos.ks@gmail.com>
2020-07-26 23:52:47 +0100

    update files

--- script/script.js
+++ script/script.js
@@ -3,20 +3,23 @@ let searchResult, statistics;
 let usersData;
 
 function start() {
+
   inputText = document.querySelector('input[type=text]')
   button = document.querySelector('button')
   searchResult = document.querySelector('.users')
   statistics = document.querySelector('.statistics')
+
+  inputText.focus()
 
   fecthUsers()
   inputActive()
 }
 
 async function fecthUsers() {
-  const users = await fetch('https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo')
+  const users = await fetch('http://localhost:3000/results')
   const json = await users.json()
 
-  usersData = json.results.map(element => {
+  usersData = json.map(element => {
     let {
       name,
       gender,
@@ -38,7 +41,10 @@ async function fecthUsers() {
 function inputActive() {
   let inputValue;
 
-  button.addEventListener('click', () => getResults(inputValue))
+  button.addEventListener('click', () => {
+    getResults(inputValue)
+    inputText.value = ''
+  })
 
   inputText.addEventListener('keyup', e => {
     inputValue = inputText.value.toLowerCase()
@@ -46,25 +52,73 @@ function inputActive() {
       return
     }
     getResults(inputValue)
+    inputText.value = ''
   })
 }
 
 function getResults(inputValue) {
-  let usersResult = usersData.filter(element => {
-    return element.name.toLowerCase().includes(inputValue) == true
-  })
-  console.log(usersResult)
+  let usersResult;
 
-  render(usersResult)
+  if (inputValue.length >= 1) {
+
+    usersResult = usersData.filter(element => {
+      return element.name.toLowerCase().includes(inputValue) == true
+    })
+    render(usersResult)
+
+  } else if (inputValue === '') {
+
+    usersResult = usersData
+    render(usersResult)
+
+  }
 }
 
 function render(usersResult) {
-  searchResult.innerHTML = `
-  <img src="${usersResult.picture}" alt="foto"> 
-  <span>${usersResult.name}</span>, 
-  <span>${usersResult.age}</span>
-  `
-}
+  console.log(usersResult)
+  searchResult.innerHTML = `<h2>${usersResult.length} Usuarios encontrados</h2>`
+  let divResults = document.createElement('div')
 
+  let sumAge = usersResult.map(element => element.age).reduce((acc, cur) => {
+    return acc += cur
+  })
+  let middleAge = usersResult.map(element => element.age).reduce((acc, cur) => {
+    return (sumAge / usersResult.length).toFixed(2)
+  })
+  let userFemale = usersResult.filter(element => element.gender === 'female')
+  let userMale = usersResult.filter(element => element.gender === 'male')
+
+  console.log(sumAge)
+
+  usersResult.forEach(element => {
+    let img = element.picture
+    let name = element.name
+    let age = element.age
+    let gender = element.gender
+
+    let userDiv = document.createElement('div');
+
+    userDiv.innerHTML = `
+      <img src="${img}" alt="">
+      <span>${name}</span>,
+      <span>${age}</span>
+    `
+    statistics.innerHTML = `
+    <h2>Estatisticas</h2>
+    <span>Mulheres: ${userFemale.length}</span><br>
+    <span>Homens: </span>${userMale.length}<br>
+    <span>Soma das idades: ${sumAge}</span><br>
+    <span>Media das idades: ${middleAge} </span><br>
+    `
+    console.log(element)
+
+    divResults.appendChild(userDiv)
+
+
+  });
+
+  searchResult.appendChild(divResults)
+
+}
 
 start()
